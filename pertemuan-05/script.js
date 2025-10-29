17b2017548d979cf5746973032fcfb0fc647c36d
unknown <2511500083@mahasiswa.atmaluhur.ac.id>
2025-10-30 00:50:27 +0700

    memperbaiki script js(form)

--- pertemuan-05/script.js
+++ pertemuan-05/script.js
@@ -94,7 +94,7 @@ document.addEventListener("DOMContentLoaded", function () {
 document.querySelector("form").addEventListener("submit", function (e) {
   const nama = document.getElementById("txtNama");
   const email = document.getElementById("txtEmail");
-  const pesan = document.getElementById("txtPesan");
+  const pesan = document.getElementById("txtPesan"); 
 
   document.querySelectorAll(".error-msg").forEach(el => el.remove());
   [nama, email, pesan].forEach(el => el.style.border = "");

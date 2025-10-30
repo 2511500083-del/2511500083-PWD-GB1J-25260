80f610ec888621490c6dbd7f501750266ea2d7e8
unknown <2511500083@mahasiswa.atmaluhur.ac.id>
2025-10-30 21:03:17 +0700

    skrip php selamat datang

--- pertemuan-06/README.md
+++ pertemuan-06/README.md
@@ -7,4 +7,5 @@ Hari ini, Kamis 23 Oktober 2025, saya belajar:
 <ol>
   <li>skrip php hello world</li>
   <li>copy index.html, style.css, script.js (pertemuan-06)</li>
+  <li>skrip php selamat datang</li>
 </ol>  
---
+++ pertemuan-06/latihan1.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Belajar PHP Dasar</title>
+</head>
+<body>
+    <h1><?php echo "Halo, Dunia PHP!"; ?></h1>
+    <h2><?php echo "Selamat Datang di PHP, Forum Salsabilla!"; ?></h2>
+</body>
+</html>

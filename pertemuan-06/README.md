6d75bd9bf04ba29ee46de67a6706b60a71102764
unknown <2511500083@mahasiswa.atmaluhur.ac.id>
2025-10-30 21:08:08 +0700

    skrip variabel dan tipe data

--- pertemuan-06/README.md
+++ pertemuan-06/README.md
@@ -8,4 +8,5 @@ Hari ini, Kamis 23 Oktober 2025, saya belajar:
   <li>skrip php hello world</li>
   <li>copy index.html, style.css, script.js (pertemuan-06)</li>
   <li>skrip php selamat datang</li>
+  <li>skrip variabel dan tipe data</li>
 </ol>  
---
+++ pertemuan-06/latihanvariabel.php
@@ -0,0 +1,15 @@
+<?php
+$nama = "Salsabilla";
+$umur = 18;
+$tinggi = 1.55;
+$aktif = true;
+
+echo "Nama: $nama <br>";
+echo "Umur: $umur tahun <br>";
+echo "Tinggi: $tinggi meter <br>";
+echo "Status aktif: " . ($aktif ? "Ya" : "Tidak") . "<br>";
+var_dump($nama);
+var_dump($umur);
+var_dump($tinggi);
+var_dump($aktif);
+?>

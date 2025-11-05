41f89cda0cab7a103d726f7e9033fec10f02702c
unknown <2511500083@mahasiswa.atmaluhur.ac.id>
2025-11-06 06:35:23 +0700

    latihan konstanta

--- pertemuan-06/latihankonstanta.php
+++ pertemuan-06/latihankonstanta.php
@@ -2,6 +2,6 @@
 define("DOSEN_PENGAMPU", "Bapak Yohanes Setiawan Japriadi, M.Kom.");
 const MATAKULIAH = "Pemrograman Web Dasar";
 
-echo "DOSEN PENGAMPU: " . Bapak Yohanes Setiawan Japriadi, M.Kom.  . "<br>";
+echo "DOSEN PENGAMPU: " . Bapak Yohanes Setiawan Japriadi, M.Kom . "<br>";
 echo "MATAKULIAH: " . Pemrograman Web Dasar . ;
 ?>
